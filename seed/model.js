94ab3cb00e45764857e5507a6f10b4733f5ec0ef
Jenny Nguyen <nguyencuc1120@gmail.com>
2023-06-07 13:06:12 -0500

    updated features

--- seed/model.js
+++ seed/model.js
@@ -4,82 +4,75 @@ const { Brand,Model } = require('../models')
 db.on('error', console.error.bind(console,'error:'))
 
 const main = async () => {
-    const canon = await brands.find({name: 'canon'})
-    const nikon = await brands.find({name: 'nikon'})
-    const sony = await brands.find({name: 'sony'})
-
+    const canon = await Brand.findOne({brand: 'canon'})
+    const nikon = await Brand.findOne({brand: 'nikon'})
+    const sony = await Brand.findOne({brand: 'sony'})
+ 
     const model = [
         {
         image:"https://s7d1.scene7.com/is/image/canon/3924C001_eos-rebel-t8i-(body-only)_primary?fmt=webp-alpha&wid=800",
-        brandName: 'Canon',
+        brandName: canon._id,
         model: 'EOS Rebel T8i',
-        price: '$749.99',           
-        model_id: canon[0]._id
+        price: 749.99,           
         },
         {
         image:"https://s7d1.scene7.com/is/image/canon/1483C002_eos-5d-mark-iv_1NEW?fmt=webp-alpha&wid=800",
-        brandName: 'Canon',
+        brandName: canon._id,
         model:'EOS 5D Mark IV',
-        price: '$2,499.00',           
-        model_id: canon[0]._id
+        price: 2499.00,           
+        model_id: canon._id
         },
         {
         image:"https://s7d1.scene7.com/is/image/canon/3829C019_primary?fmt=webp-alpha&wid=800",
-        brandName: 'Canon',
+        brandName: canon._id,
         model: 'EOS-1D X Mark III',
-        price: '$6,499.00',          
-        model_id: canon[0]._id
+        price: 6499.00,          
         },
         {
         image:"https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/nikon_d6_dslr_camera_body_1581510285_1515330.jpg",
-        brandName: 'Nikon',
+        brandName: nikon._id,
         model: 'Nikon D6 DSLR',
-        price: '$6,496.95',
-        brands_id: nikon[0]._id
+        price: 6496.95,
         },
         {
         image:"https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/nikon_d_610_digital_slr_body_1381189748_1008264.jpg",
-        brandName: 'Nikon'
+        brandName: nikon._id,
         model: 'Nikon D610 DSLR',
-        price: '$1,099.00',
-        brands_id: nikon[0]._id
+        price: 1099.00,
         },
         {
         image:"https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/nikon_d850_dslr_camera_body_1503543023_1351688.jpg",
-        brandName: 'Nikon',
+        brandName: nikon._id,
         model: 'Nikon D850 DSLR',
-        price: '$2,496.95',
-        brands_id: nikon[0]._id
+        price: 2496.95,
         },
         {
         image:"image: https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_alpha_a7c_mirrorless_digital_1600118443_1592776.jpg",
-        brandName: 'Sony',
+        brandName: sony._id,
         model: 'Sony a7C Mirrorless',
-        price: '$1,598.00',
-        brands_id: sony[0]._id
+        price: 1598.00,
         },
         {
         image:"https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_zv_e10_mirrorless_camera_with_1627386359_1655301.jpg",
-        brandName: 'Sony',
+        brandName: sony._id,
         model: 'Sony ZV-E10 Mirrorless',
-        price: '$798.00',
-        brands_id: sony[0]._id
+        price: 798.00,
         },
         {
         image:"https://www.bhphotovideo.com/cdn-cgi/image/format=auto,fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/sony_zv_e1_mirrorless_camera_black_1680084933_1759472.jpg",
-        brandName: 'Sony',
+        brandName: sony._id,
         model: 'Sony ZV-E1 Mirrorless ',
-        price: '$2,198.00',
-        brands_id: sony[0]._id
+        price: 2198.00,
         }
 
     ]
+    
     await Model.insertMany(model)
     console.log('created model')
 }
 
 const run = async() => {
     await main()
-    db close()
+    db.close()
 }
 run()
