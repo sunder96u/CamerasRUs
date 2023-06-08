174b748e39d5bc9106b179b582355f6bb5b8b7fb
Jenny Nguyen <nguyencuc1120@gmail.com>
2023-06-08 10:17:39 -0500

    updated readme

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const db = require('./db')
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT || 3001
 const AppRouter = require('./routes/appRouter')
 const landingPage = require('./controllers/brandController')
 const cors = require('cors')
